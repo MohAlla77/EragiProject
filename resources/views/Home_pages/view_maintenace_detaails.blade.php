e43619333833685f532e95d566a1c4cefbbb1f3f
MohAlla77 <moh.allaaadeen@gmail.com>
2024-03-25 00:14:59 +0300

    fix maintenance

--- resources/views/Home_pages/view_maintenace_detaails.blade.php
+++ resources/views/Home_pages/view_maintenace_detaails.blade.php
@@ -1,4 +1,55 @@
-@extends('Layout.head')
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>الرئسية</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
+    <style>
+        input[type="range"] {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 100%;
+            height: 15px;
+            /* Adjust height as needed */
+            border-radius: 5px;
+            /* Adjust border radius as needed */
+            outline: none;
+            margin: 10px 0;
+            /* Adjust margin as needed */
+        }
+
+        input[type="range"]::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 20px;
+            /* Adjust thumb width as needed */
+            height: 20px;
+            /* Adjust thumb height as needed */
+            border-radius: 50%;
+            /* Round thumb */
+            background: white;
+            /* Thumb color */
+            cursor: pointer;
+        }
+        .logo-img {
+            width: 60px;
+            /* Adjust the width as needed */
+            height: auto;
+            /* Maintain aspect ratio */
+            margin-right: 20px;
+            /* Adjust the margin as needed */
+        }
+    </style>
+</head>
 
 <body class="sb-nav-fixed">
     @include('Layout.navbar')
@@ -87,4 +138,15 @@
             @include('Layout.footer')
         </div>
     </div>
+
+
+    <canvas id="myChart"></canvas>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+        crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
 </body>
