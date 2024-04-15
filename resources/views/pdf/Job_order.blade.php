3891836600b51bedf7c42a81a71936154bdf095f
Eragi <hatim.eleragi@gmail.com>
2024-04-15 10:48:48 +0300

    edit

---
+++ resources/views/pdf/Job_order.blade.php
@@ -0,0 +1,22 @@
+<div class="rtl">
+    <div class="logo-container">
+        <img style="height: 100px" src="logo.jpg">
+    </div>
+ <div  style="direction: rtl;">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
