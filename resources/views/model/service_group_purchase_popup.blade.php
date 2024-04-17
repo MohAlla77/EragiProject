1bdf3026b400c53b4e2cec70a8aee0a1da1eb661
Eragi <hatim.eleragi@gmail.com>
2024-04-17 12:20:14 +0300

    1/done model item & service in purchases

--- resources/views/model/service_group_purchase_popup.blade.php
+++ resources/views/model/service_group_purchase_popup.blade.php
@@ -1,31 +1,31 @@
-
-  
-  <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">ادخل اسم المجموعة</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <div class="col-12">
+            <h5 class="modal-title text-center" id="exampleModalLabel">ادخل اسم المجموعة</h5>
+          </div>
         </div>
-        <form class="row g-1" id="addItemForm" novalidate
-        action="{{ route('ServiceGroup.store') }}" method="post">
-        @csrf
-        <div class="modal-body">
-            <div class="col-md-12">
+        <form id="addItemForm" novalidate
+          action="{{ route('ServiceGroup.store') }}" method="post">
+          @csrf
+          <div class="modal-body">
+            <div class="col-md-12 mb-1">
                 <input type="text" class="form-control text-center"
                     id="AddagroupFields" required
                     placeholder="اضافة المجموعة الخدمات" readonly>
             </div>
-            <div class="col-md-6">
-                <input name="GroupName" type="text"
-                    class="form-control text-center" id="#" required
-                    placeholder="اسم المجموعة">
-            </div>
-            <div class="col-md-6">
-                <input name="GroupNumber" type="text"
-                    class="form-control text-center" id="#" required
-                    placeholder="رقم المجوعة">
+            <div class="row g-1">
+              <div class="col-md-6 mb-1">
+                  <input name="GroupName" type="text"
+                      class="form-control text-center" id="#" required
+                      placeholder="اسم المجموعة">
+              </div>
+              <div class="col-md-6">
+                  <input name="GroupNumber" type="text"
+                      class="form-control text-center" id="#" required
+                      placeholder="رقم المجوعة">
+              </div>
             </div>
             <div class="col-md-12">
                 <input name="GroupID" type="text"
@@ -33,11 +33,16 @@
                     required placeholder="أضف رمز (Ex: الماكنيكا -> MCH)"
                     required>
             </div>
-                </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
+          </div>
+          <div class="modal-footer">
+            <dic class="col-sm-12 text-center">
+              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">
+                اضافة
+                <i class="fa-solid fa-plus"></i>
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     </div>
